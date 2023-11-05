5493907bc2a9bf1c76e7119c7682c45f17ff50d9
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2023-11-05 22:38:35 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const bodyParser = require("body-parser")
+const app = express()
+
+app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200'],
+}))
+
+app.use(bodyParser.json({limit: '50mb' }));
+app.use(bodyParser.urlencoded({extended:true, limit: '50mb', parameterLimit:50000}))
+app.use(express.json());
+app.use(bodyParser.json());
+app.use("/api", routes)
+
+
+const port = process.env.PORT || 5000;
+mongoose.connect("mongodb+srv://superAdmin:comSuperAdmin@cluster0.2ecyphf.mongodb.net/onboarder?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+ 
+.then(()=>{
+    console.log("connected to db")
+
+    app.listen(port, ()=> {
+        console.log("app is listening on port 5000")
+    })
+})
+
+
+
+
